5d8a3604c17787f63a78f53c1b51f0ff8cd15ad8
Mario Repas <mario.repas63@gmail.com>
2023-01-22 19:39:01 -0500

    added question functionality

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,76 @@
 // Selects element by class
 let timeEl = document.querySelector("#secLeft");
 let startButton = document.querySelector("#startGame")
+let leftCard = document.querySelector(".left")
 // Global variables
 let secondsLeft = 5;
 let correctAnswers = "";
 let wrongAnswers = "";
+let counter = 0;
 let theQuestions = [
     {
-        question1: "What year was javascript founded?",
+        question: "What year was javascript founded?",
 
-        answers1: {
+        answers: {
             a: '1985',
             b: '1995',
             c: '2005',
             d: '2015',
         },
 
-        correctAnswer1: 'b'
+        correctAnswer: 'b'
     },
 
     {
-        question2: "What notation is used to declare a variable?",
+        question: "What notation is used to declare a variable?",
 
-        answers2: {
+        answers: {
             a: "let",
             b: "bet",
             c: "set",
             d: "pet",
         },
 
-        correctAnswer2: "a"
+        correctAnswer: "a"
     },
 
     {
-        question3: "What is a bug in javascript?",
+        question: "What is a bug in javascript?",
 
-        answers3: {
+        answers: {
             a: "an insect on screen",
             b: "a nagging partner",
             c: "anything that causes the program to run incorrectly",
             d: "a hidden microphone",
         },
 
-        correctAnswer3: "c"
+        correctAnswer: "c"
 
     },
 ]
-
+// for (let i = 0; i < x; i++) {
+//       // Enter code that will get run ‘x’ amount of times
+//     }
 function showQuestions() {
+    let activeQuestion = theQuestions[counter]
+    let questionElement = document.createElement("p");
+    questionElement.setAttribute("class","question");
+    questionElement.textContent = activeQuestion.question;
+    leftCard.appendChild(questionElement);
+}
 
+function showAnswers() {
+    let activeAnswer = theQuestions[counter];
+    let answerElement = document.createElement("p");
+    answerElement.setAttribute("class","answer");
+    answerElement.textContent = activeAnswer.answers;
+    leftCard.appendChild(answerElement);
 }
 
 function gameStart() {
     setTime();
-
+    showQuestions();
+    showAnswers();
 }
 
 function setTime() {
