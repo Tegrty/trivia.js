87732c334141a1b1178461edb0f649c82054f176
Mario Repas <mario.repas63@gmail.com>
2023-01-24 09:31:56 -0500

    added questions and timer functionality

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,11 @@
 let timeEl = document.querySelector("#secLeft");
 let startButton = document.querySelector("#startGame")
 let leftCard = document.querySelector(".left")
+let answerContainer = document.querySelector(".answer-container")
+let cards = document.querySelector(".card")
+let input = document.querySelector("#input")
 // Global variables
-let secondsLeft = 5;
+let secondsLeft = 15;
 let correctAnswers = "";
 let wrongAnswers = "";
 let counter = 0;
@@ -11,46 +14,44 @@ let theQuestions = [
     {
         question: "What year was javascript founded?",
 
-        answers: {
-            a: '1985',
-            b: '1995',
-            c: '2005',
-            d: '2015',
-        },
+        answers: [
+             '1985',
+             '1995',
+             '2005',
+             '2015',
+        ],
 
-        correctAnswer: 'b'
+        correctAnswer: '1995'
     },
 
     {
         question: "What notation is used to declare a variable?",
 
-        answers: {
-            a: "let",
-            b: "bet",
-            c: "set",
-            d: "pet",
-        },
+        answers: [
+             "let",
+             "bet",
+             "set",
+             "pet",
+    ],
 
-        correctAnswer: "a"
+        correctAnswer: "let"
     },
 
     {
         question: "What is a bug in javascript?",
 
-        answers: {
-            a: "an insect on screen",
-            b: "a nagging partner",
-            c: "anything that causes the program to run incorrectly",
-            d: "a hidden microphone",
-        },
+        answers: [
+             "an insect on screen",
+             "a nagging partner",
+             "anything that causes the program to run incorrectly",
+             "a hidden microphone",
+        ],
 
-        correctAnswer: "c"
+        correctAnswer: "anything that causes the program to run incorrectly"
 
     },
 ]
-// for (let i = 0; i < x; i++) {
-//       // Enter code that will get run ‘x’ amount of times
-//     }
+
 function showQuestions() {
     let activeQuestion = theQuestions[counter]
     let questionElement = document.createElement("p");
@@ -61,10 +62,49 @@ function showQuestions() {
 
 function showAnswers() {
     let activeAnswer = theQuestions[counter];
-    let answerElement = document.createElement("p");
+    for (var i = 0; i < activeAnswer.answers.length; i++) {
+         let answerElement = document.createElement("button");
     answerElement.setAttribute("class","answer");
-    answerElement.textContent = activeAnswer.answers;
+    answerElement.textContent = activeAnswer.answers[i]
     leftCard.appendChild(answerElement);
+    answerElement.addEventListener("click", checkAnswer);
+
+    }
+   
+function checkAnswer(event) {
+    let activeAnswer = theQuestions[counter];
+    if (event.target.textContent != activeAnswer.correctAnswer) {
+        alert("incorrect answer")
+        secondsLeft = secondsLeft - 5
+        wrongAnswers = wrongAnswers + 1
+    }
+    else {
+        alert("correct answer")
+        correctAnswers = correctAnswers +1
+    }
+    counter++
+    showQuestions();
+    showAnswers();
+
+}
+
+
+    // let list = document.createElement('ul')
+
+    // for (var i = 0; i < answers.length; i++) {
+    //     var item = document.createElement('li');
+
+    //     item.appendChild(document.textContent(answers[i]));
+
+    //     list.appendChild(item);
+    // }
+
+
+}
+
+function gameOver() {
+    cards.setAttribute("class", "hide-element");
+    input.removeAttribute("class");
 }
 
 function gameStart() {
@@ -79,8 +119,9 @@ function setTime() {
     secondsLeft--; //Decrements time left by 1
     timeEl.textContent = secondsLeft + " seconds remaining until failure.";
 
-    if(secondsLeft === 0) {
-        timeEl.textContent = "Failure"
+    if(secondsLeft <= 0) {
+        timeEl.textContent = "Enter your initials for High score"
+        gameOver()
       // Stops execution of action at set interval
       clearInterval(timerInterval);
     
